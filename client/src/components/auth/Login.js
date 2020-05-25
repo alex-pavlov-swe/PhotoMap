c6982f07fcbea635fc89c7cb256393165e39a2f4
PavlovAS <pavlov.cmc@gmail.com>
2020-05-24 21:22:07 -0700

    New Login design

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,14 +1,21 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { login } from '../../actions/auth';
 import PropTypes from 'prop-types';
 import { Redirect, Link } from 'react-router-dom';
+import Footer from '../layout/Footer';
 
 function Login({ login, isAuthenticated, lang: { lang } }) {
   const [formData, setFormData] = useState({
     email: '',
     password: '',
   });
+
+  useEffect(() => {
+    if (document.getElementById('login-form')) {
+      document.getElementById('login-form').classList.add('show');
+    }
+  }, []);
 
   const { email, password } = formData;
 
@@ -28,68 +35,58 @@ function Login({ login, isAuthenticated, lang: { lang } }) {
 
   return (
     <Fragment>
-      <div className="row mt-4 mb-2 login-container full-page">
-        <div className="col-md-6 offset-md-3 col-sm-12">
-          <form className="form" onSubmit={(e) => onSubmit(e)}>
-            <h3>
-              <i className="fas fa-user" />
-              {lang === 'rus' ? 'Войти в аккаунт' : 'Log in'}
-            </h3>
-            <div className="form-group">
-              <label htmlFor="email">Email</label>
-              <input
-                type="text"
-                name="email"
-                value={email}
-                onChange={(e) => onChange(e)}
-                required
-                placeholder={
-                  lang === 'rus' ? 'Введите e-mail' : 'Enter your email'
-                }
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="password">
-                {lang === 'rus' ? 'Пароль' : 'Password'}
-              </label>
-              <input
-                type="text"
-                name="password"
-                value={password}
-                onChange={(e) => onChange(e)}
-                required
-                placeholder={
-                  lang === 'rus' ? 'Введите пароль' : 'Enter your password'
-                }
-              />
-            </div>
-            <button className="btn btn-info mt-1 mb-4" type="submit">
-              {lang === 'rus' ? 'Войти' : 'Submit'}
-            </button>
-            <p>
-              {lang === 'rus'
-                ? 'У Вас еще нет аккаунта?'
-                : 'Dont have an account?'}{' '}
-              <Link to="register">
-                {lang === 'rus' ? 'Создайте его,' : 'Create one'}
-              </Link>{' '}
-              {lang === 'rus'
-                ? 'чтобы туркомпании могли с Вами связаться.'
-                : 'to submit your profile and start being visible for potential employers'}
-            </p>
-          </form>
-          <p>
-            {lang === 'rus'
-              ? 'Нащ сервис может помочь начти работу следующим категориям:'
-              : ''}
-          </p>
-          <p>
-            {lang === 'rus'
-              ? 'гид, повар, переводчик, помощник гида, волотнер, водитель вахтовки, автобуса, джипа, микроавтобуса, водитель-гид на легковом автомобиле.'
-              : ''}
-          </p>
+      <div className="container-fluid" id="login">
+        <div className="row">
+          <div className="col-sm-12 text-center">
+            <form
+              className="form hide"
+              onSubmit={(e) => onSubmit(e)}
+              id="login-form"
+            >
+              <h3>
+                <i className="fas fa-user mb-3" />
+                {lang === 'rus' ? 'Войти в аккаунт' : 'Log in'}
+              </h3>
+              <div className="form-group">
+                <input
+                  type="text"
+                  name="email"
+                  value={email}
+                  onChange={(e) => onChange(e)}
+                  required
+                  placeholder={
+                    lang === 'rus' ? 'Введите e-mail' : 'Enter your email'
+                  }
+                />
+              </div>
+              <div className="form-group">
+                <input
+                  type="text"
+                  name="password"
+                  value={password}
+                  onChange={(e) => onChange(e)}
+                  required
+                  placeholder={
+                    lang === 'rus' ? 'Введите пароль' : 'Enter your password'
+                  }
+                />
+              </div>
+              <div className="form-group">
+                <button className="btn btn-primary mt-1 mb-4" type="submit">
+                  {lang === 'rus' ? 'Войти' : 'Sign in'}
+                </button>
+              </div>
+              <p>
+                {lang === 'rus' ? 'Еще нет аккаунта?' : 'Not a member?'}{' '}
+                <Link to="register">
+                  {lang === 'rus' ? 'Создайте его.' : 'Sign up here.'}
+                </Link>
+              </p>
+            </form>
+          </div>
         </div>
       </div>
+      <Footer />
     </Fragment>
   );
 }
