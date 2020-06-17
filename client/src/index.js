6d16a780855982aec907a555402270c9b4e4ee70
PavlovAS <pavlov.cmc@gmail.com>
2020-06-16 20:16:40 -0700

    revise

--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import './css/style.css';
 import './scss/index.scss';
-import './css/style_small.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
