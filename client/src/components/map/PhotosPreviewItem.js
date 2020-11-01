965ed3a6aca7ec5d739828567d68b55f672e7bf2
PavlovAS <pavlov.cmc@gmail.com>
2020-11-01 10:04:11 -0800

    tune open photo modal for  a class component

--- client/src/components/map/PhotosPreviewItem.js
+++ client/src/components/map/PhotosPreviewItem.js
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
 import Modal from 'react-modal';
 import PhotoModal from '../../modals/PhotoModal';
+import './styles.css';
 
 export class PhotosPreviewItem extends React.Component {
     constructor() {
@@ -16,19 +17,28 @@ export class PhotosPreviewItem extends React.Component {
 
     open() {
         this.setState({ showModal: true });
+        let itemsToHide = document.getElementsByClassName('mapboxgl-ctrl');
+        for (let i = 0; i < itemsToHide.length; i++) {
+            itemsToHide[i].style.display = 'none';
+        }
     }
 
     close() {
         this.setState({ showModal: false });
+        let itemsToHide = document.getElementsByClassName('mapboxgl-ctrl');
+        for (let i = 0; i < itemsToHide.length; i++) {
+            itemsToHide[i].style.display = 'block';
+        }
     }
 
     render() {
         return (
             <Fragment>
-                    <img onClick={this.open} src={this.props.photo.url} alt="photo"/>
+                <img onClick={this.open} src={this.props.photo.url} alt="photo"/>
                 <Modal
                     isOpen={this.state.showModal}
                     contentLabel="Minimal Modal Example"
+                    className="modal d-block"
                 >
                     <PhotoModal photoId={this.props.photo._id} close={this.close}></PhotoModal>
                 </Modal>
--- client/src/components/profile/ProfilePhotoItem.js
+++ client/src/components/profile/ProfilePhotoItem.js
@@ -1,12 +1,41 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { Fragment } from 'react';
+import Modal from 'react-modal';
+import PhotoModal from '../../modals/PhotoModal';
+import './styles.css';
 
-const ProfilePhotoItem = ({ photo: photo }) => {
-    return (
-        <Link to={`/photo/${photo._id}`} photo={photo.title}>
-            <img src={photo.url} alt="photo"/>
-        </Link>
-    )
+export class ProfilePhotoItem extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            showModal: false
+        };
+        this.open = this.open.bind(this);
+        this.close = this.close.bind(this);
+    }
+
+    open() {
+        this.setState({ showModal: true });
+    }
+
+    close() {
+        this.setState({ showModal: false });
+    }
+
+    render() {
+        const { photo } = this.props;
+        return (
+            <Fragment>
+                <img onClick={this.open} src={this.props.photo.url} alt="photo"/>
+                <Modal
+                    isOpen={this.state.showModal}
+                    contentLabel="Minimal Modal Example"
+                    className="modal d-block"
+                >
+                    <PhotoModal photoId={this.props.photo._id} close={this.close}></PhotoModal>
+                </Modal>
+            </Fragment>
+        )
+    }
 }
 
 export default ProfilePhotoItem
