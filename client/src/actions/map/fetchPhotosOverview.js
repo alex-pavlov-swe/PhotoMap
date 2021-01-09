bb22208d3271e84b16fb1b112bb438435445f582
PavlovAS <pavlov.cmc@gmail.com>
2021-01-08 21:34:41 -0800

    restore get map overview for web

--- client/src/actions/map/fetchPhotosOverview.js
+++ client/src/actions/map/fetchPhotosOverview.js
@@ -6,26 +6,26 @@ import {
 
 // Fetch photos from Mongo to show on a map overview
 export const fetchPhotosOverview = (bounds) => async (dispatch) => {
-	const config = {
-		headers: {
-			'Content-Type': 'application/json',
-		},
+    const config = {
+        headers: {
+            'Content-Type': 'application/json',
+        },
     };
     const body = JSON.stringify({ bounds });
 
-	try {
-		const res = await axios.post(`/api/map/photos/overview`, body, config);
+    try {
+        const res = await axios.post(`/api/map/photos/overview/web`, body, config);
 
-		dispatch({
-			type: MAP_PHOTOS_OVERVIEW_LOADED,
-			payload: res.data,
+        dispatch({
+            type: MAP_PHOTOS_OVERVIEW_LOADED,
+            payload: res.data,
         });
 
         return res;
-	} catch (error) {
-		dispatch({
-			type: MAP_PHOTOS_OVERVIEW_LOADING_ERROR,
-			payload: error,
-		});
+    } catch (error) {
+        dispatch({
+            type: MAP_PHOTOS_OVERVIEW_LOADING_ERROR,
+            payload: error,
+        });
     }
 };
